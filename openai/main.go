4176865fa00411b0624d9bb3b325d64e7581cf73
wangqian <wq247934918@gmail.com>
2023-03-20 10:12:50 +0800

    chatGpt demo

--- openai/main.go
+++ openai/main.go
@@ -3,22 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
-	gogpt "github.com/sashabaranov/go-gpt3"
+	openai "github.com/sashabaranov/go-openai"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	c := gogpt.NewClient("")
-	ctx := context.Background()
+	v := viper.GetViper()
+	v.SetConfigFile("config.yaml")
+	v.GetString("openai.sk")
+	config := openai.DefaultConfig(v.GetString("openai.sk"))
+	config.BaseURL = v.GetString("openai.base_url")
+	client := openai.NewClientWithConfig(config)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
 
-	req := gogpt.CompletionRequest{
-		Model:     gogpt.GPT3Dot5Turbo,
-		MaxTokens: 5,
-		Prompt:    "Lorem ipsum",
-	}
-	resp, err := c.CreateCompletion(ctx, req)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("ChatCompletion error: %v\n", err)
 		return
 	}
-	fmt.Println(resp.Choices[0].Text)
+
+	fmt.Println(resp.Choices[0].Message.Content)
 }
