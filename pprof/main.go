b3b497cc3ef604c638ae38ec1a030ce81d3805a9
wangqian <wq247934918@gmail.com>
2023-03-21 23:57:29 +0800

    test pprof

---
+++ pprof/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+	"sync/atomic"
+)
+
+var visitors int64
+
+func handleHi(w http.ResponseWriter, r *http.Request) {
+
+	if match, _ := regexp.MatchString(`^\w*$*`, r.FormValue("color")); !match {
+
+		http.Error(w, "Optional color is invalid", http.StatusBadRequest)
+
+		return
+
+	}
+
+	visitNum := atomic.AddInt64(&visitors, 1)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	content := []byte("<h1 style='color:" + r.FormValue("color") + "' >Welcome !</h1>You are visitor number" +
+		fmt.Sprint(visitNum) + "!")
+	fmt.Println(string(content))
+	w.Write(content)
+}
+
+func main() {
+
+	log.Printf("Starting on port 8080")
+
+	http.HandleFunc("/hi", handleHi)
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+
+}
